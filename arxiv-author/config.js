a0a779e37668926af2e1fe342f6dba8422622383
Junnosuke Koizumi <jun.juggler@gmail.com>
2026-05-01 22:39:22 +0900

    no message

--- arxiv-author/config.js
+++ arxiv-author/config.js
@@ -2,5 +2,5 @@
 // Cloudflare Workersなどに置いた arXiv APIプロキシのURLを設定します。
 //
 // 例:
-// window.ARXIV_API_ENDPOINT = "https://your-worker.your-name.workers.dev/api/arxiv";
+// window.ARXIV_API_ENDPOINT = "https://arxiv-author-categories-proxy.jun-juggler.workers.dev/api/arxiv";
 window.ARXIV_API_ENDPOINT = "";
